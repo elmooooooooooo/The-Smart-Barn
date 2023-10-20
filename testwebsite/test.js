28bc5d8a9d22d739fb6178f8f67388d5ca976cb1
semerded <sem.van.broekhoven@gmail.com>
2023-10-20 13:56:44 +0200

    puhsh

--- testwebsite/test.js
+++ testwebsite/test.js
@@ -1,4 +1,4 @@
-const dataURL = "http://172.16.1.107:5000"
+const dataURL = "http://172.16.1.107:5000";
 function requestData() {
     const fetchPromise = fetch(dataURL);
     fetchPromise.then(response => {
@@ -10,4 +10,13 @@ function requestData() {
 
 function transferData(data) {
     document.getElementById("text").innerHTML = data;
+    dataArray = Object.entries(data)
+    console.log(dataArray)
+    writeCSV(dataArray);
+}
+
+function writeCSV(data) {
+    let csvContent = "data:text/csv;charset=utf-8" + data.map(e => e.join(",")).join("\n");
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
 }
