cf9662e5c1de0343fc84eb8e1ef48f008df08189
semerded <sem.van.broekhoven@gmail.com>
2024-01-18 20:33:21 +0100

    werkende graph

--- software/main.py
+++ software/main.py
@@ -19,7 +19,12 @@ CORS(APP, resources={r"/*": {"origins": "*"}})
 
 JSONFILEPATH = "software/setup.json"
 if os.path.isfile(JSONFILEPATH) is False:
-    raise Exception("JSON setup file not found in app directory")
+    raise FileNotFoundError("JSON setup file not found in app directory")
+
+class JsonFileIsEmpty(Exception): ...
+if os.path.getsize(JSONFILEPATH) < 10:
+    raise JsonFileIsEmpty("The Setup file seem to be empty")
+
 
 def randomAlgorithm(previousNumber):
     for i in range(random.randint(1,20)):
@@ -133,4 +138,4 @@ def index():
     return jsonify(newData), 200
 
 if __name__ == "__main__":
-    APP.run()
+    APP.run(host="0.0.0.0")
