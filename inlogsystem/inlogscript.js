3a20e398f6b0de10e7558dd98d9b6e6e6555888b
Ziezamo1 <zazielmomo@gmail.com>
2024-01-30 09:08:10 +0100

    hezoiioe hoifiohz ioehfiohiozefh iofeh fioh ezfiohefoi fogz eofhiozehf oi

---
+++ inlogsystem/inlogscript.js
@@ -0,0 +1,33 @@
+
+const LoginInfo = {
+    "email" : "boer@mosa-rt.be",
+    "password" : "boer123KSG"
+} 
+
+// Function to handle login
+function login() {
+
+
+    const emailinput = document.getElementById("email").value
+    const passwordinput = document.getElementById("password").value
+
+
+
+
+
+    // Check if the provided email and password match the stored loginInfo
+    if (emailinput == LoginInfo.email && passwordinput == LoginInfo.password) {
+        window.location.href = "/software/graphhh.html"; 
+
+        console.log("Login successful!");
+        // If it works forward the user to the next page
+
+    } else {
+        
+        console.log("Login failed. Please check your credentials.");
+    }
+}
+
+
+// script is gefixt, groetjes sem
+// U+1FAE1
