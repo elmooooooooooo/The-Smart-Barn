9ed4d3ac964bd57636c5df0244f1932a419a1012
Ziezamo1 <zazielmomo@gmail.com>
2024-01-30 08:56:33 +0100

    ehu iefiozoef uozehu fzuief hiuhefuzui hf

--- inlogsystem/inlogscript.js
+++ inlogsystem/inlogscript.js
@@ -1,24 +1,33 @@
-const loginInfo = {
-    "email": "boer@mosa-rt.be",
-    "password": "boer123KSG"
-};
+
+const LoginInfo = {
+    "email" : "boer@mosa-rt.be",
+    "password" : "boer123KSG"
+} 
 
 // Function to handle login
 function login() {
+
+
     const emailinput = document.getElementById("email").value
     const passwordinput = document.getElementById("password").value
-    const email = emailinput
-    const password = passwordinput
-    console.log(email,password)
+
+
+
+
 
     // Check if the provided email and password match the stored loginInfo
-    if (email == loginInfo.email && password == loginInfo.password) {
+    if (emailinput == LoginInfo.email && passwordinput == LoginInfo.password) {
+        window.location.href = "/software/graphhh.html"; 
+
         console.log("Login successful!");
-        window.location.replace("http://www.google.com");
+        // If it works forward the user to the next page
 
     } else {
+        
         console.log("Login failed. Please check your credentials.");
     }
 }
 
+
 // script is gefixt, groetjes sem
+// U+1FAE1
