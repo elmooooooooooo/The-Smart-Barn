de1f95230ac5789a55ddc1b14b669864efba9aa9
semerded <sem.van.broekhoven@gmail.com>
2024-02-23 12:04:50 +0100

    navbar update

--- HergebruikendeCodeSite/navbar.js
+++ HergebruikendeCodeSite/navbar.js
@@ -5,3 +5,16 @@ fetch("../HergebruikendeCodeSite/navbar.html")
   .then(data => {
     document.querySelector("navigatiebalk").innerHTML = data;
   });
+
+function setActiveNavButton(currentPage)
+{
+    if (document.getElementById(currentPage) == null) {
+        setTimeout(() => {
+            setActiveNavButton(currentPage);
+        }, 100);
+    } else {
+        document.querySelector(`#${currentPage}`).classList.remove("navbutton");
+        document.querySelector(`#${currentPage}`).classList.add("navbutton_active");
+        
+    }
+}
